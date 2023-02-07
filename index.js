2a00e01a63b9f755e310f5dc4c6e6f2f79b44082
Jack Medrek <jackmedrek@Jacks-MacBook-Pro.local>
2023-02-06 20:04:34 -0500

    Added dark mode!

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ navLinks.forEach(link => {
         document.body.classList.remove("nav-open");
     })
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const themeStylesheet = document.getElementById("theme");
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+        themeStylesheet.href = storedTheme;
+    }
+
+    const themeToggle = document.getElementById("theme-toggle");
+    themeToggle.addEventListener('click', () => {
+        // if it's light -> go dark
+        if (themeStylesheet.href.includes('light')) {
+        themeStylesheet.href = 'style/dark-theme.css';
+        themeToggle.innerText = 'Switch to light mode';
+        } else {
+        // if it's dark -> go light
+        themeStylesheet.href = 'style/light-theme.css';
+        themeToggle.innerText = 'Switch to dark mode';
+        }
+
+        localStorage.setItem("theme", themeStylesheet.href);
+    })
+})
